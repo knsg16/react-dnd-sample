e8da7dce729bd26e9595f84ce327bae89d09c4d9
Yuuki Kanasugi <knsg16@gmail.com>
2021-01-25 15:43:48 +0900

    Droppable

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,27 +1,25 @@
 import React from 'react'
-import Square from './Square'
 import Knight from './Knight'
-import { moveKnight } from './Game'
 import { DndProvider } from 'react-dnd'
 import { HTML5Backend } from 'react-dnd-html5-backend'
+import BoardSquare from './BoardSquare'
 
-
-function renderSquare(i, [knightX, knightY]) {
+function renderSquare(i, knightPosition) {
   const x = i % 8
   const y = Math.floor(i / 8)
-  const isKnightHere = x === knightX && y === knightY
-  const black = (x + y) % 2 === 1
-  const piece = isKnightHere ? <Knight /> : null
-
   return (
-    <div key={i} style={{ width: '12.5%', height: '12.5%' }} onClick={() => handleSquareClick(x, y)}>
-      <Square black={black}>{piece}</Square>
+    <div key={i} style={{ width: '12.5%', height: '12.5%' }}>
+      <BoardSquare x={x} y={y}>
+        {renderPiece(x, y, knightPosition)}
+      </BoardSquare>
     </div>
   )
 }
 
-function handleSquareClick(toX, toY) {
-  moveKnight(toX, toY)
+function renderPiece(x, y, [knightX, knightY]) {
+  if (x === knightX && y === knightY) {
+    return <Knight />
+  }
 }
 
 export default function Board({ knightPosition }) {
