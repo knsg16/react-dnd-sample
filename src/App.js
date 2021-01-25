8c8266c69940b737b68675a1ef20cd34ff8f2d10
Yuuki Kanasugi <knsg16@gmail.com>
2021-01-25 14:50:39 +0900

    Squere

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import Knight from '../src/components/Knight'
+import Squere from '../src/components/Squere'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Squere>
+        <Knight/>
+      </Squere>
     </div>
   );
 }
